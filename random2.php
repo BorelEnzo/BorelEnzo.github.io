886e8e420ee83a1fd081e628bf730cfadec0a0a5
Borel Enzo <borelenzo@gmail.com>
2018-03-20 13:07:05 +0100

    Update random2.php

--- random2.php
+++ random2.php
@@ -1,1 +1,25 @@
-{{self}}
+<html>
+	<head>
+		<title>FileStorer</title>
+		<style>
+			body {
+				background-color: #fbffd1;
+				font-family: sans-serif;
+			}
+			#container {
+				width: 40em;
+				padding: 2em;
+				border-radius: 1em;
+				border: 4px solid black;
+				background-color: #d4ffd1;
+				position: absolute;
+				top: 50%;
+				left: 50%;
+				transform: translate(-50%, -50%);
+			}
+		</style>
+	</head>
+	<body>
+	{{ user.username }}
+	</body>
+</html>
